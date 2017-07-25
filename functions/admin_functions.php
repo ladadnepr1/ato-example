a85c7f09e10c973e56ca4b163441b604023d3660
bania-bigmir <barhanof@gmail.com>
2017-07-25 22:39:22 +0300

    поправил цикл в config.php

---
+++ functions/admin_functions.php
@@ -0,0 +1,9 @@
+<?php
+echo 'file1';
+//
+//const DB_USER = 'root';
+//const DB_PASS = '';
+//// вставить имя базы данных
+//const DB_NAME = 'inform_ato_web';
+//const DB_HOST = 'localhost';
+//
--- includes/config.php
+++ includes/config.php
@@ -6,13 +6,13 @@ const DB_PASS = '';
 const DB_NAME = '';
 const DB_HOST = 'localhost';
 $path_functions=$_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'functions';
+
 $files=scandir($path_functions);
+
+
 if($files){
     foreach($files as $value){
-        include_once $value;
+        if($value!='.' and $value!='..')
+            include_once $path_functions.DIRECTORY_SEPARATOR.$value;
     }
 }
-
-
-
-
