4b4ebfc8b3e16fb77d9b893a6335137249d62e23
konovalec1488 <konovalec1488@gmail.com>
2017-07-25 20:56:24 +0300

    create form

--- includes/reg.php
+++ includes/reg.php
@@ -1,8 +1,21 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+        
+        ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Регистрация</title>
+    </head>
+    <body>
+        <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+            <label for="username">Введите ваш логин:</label>
+            <input type="text" name="name">
+            <label for="pass">Введите ваш пароль:</label>
+            <input type="password" name="pass">
+            <label for="pass">Введите ваш пароль еще раз:</label>
+            <input type="password" name="pass2">
+            <input type="submit" name="reg" value="РЕГИСТРАЦИЯ">
+        </form>
+    </body>
+</html>
