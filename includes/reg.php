67cf51a8b11b2c495d2ba6164cc29608b597fcfb
konovalec1488 <konovalec1488@gmail.com>
2017-07-25 23:13:29 +0300

    fin file

--- includes/reg.php
+++ includes/reg.php
@@ -2,13 +2,13 @@
 include_once 'config.php';
 $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
- //принимаем значения с POST
-if (isset($_POST['reg'])) {   
+//принимаем значения с POST
+if (isset($_POST['reg'])) {
     $name = mysqli_real_escape_string($dbc, trim($_POST['username']));
     $email = mysqli_real_escape_string($dbc, trim($_POST['email']));
     $pass = mysqli_real_escape_string($dbc, trim($_POST['pass']));
     $pass2 = mysqli_real_escape_string($dbc, trim($_POST['pass2']));
-    
+
     //проверка на пустые поля
     if (!empty($name) && !empty($email) && !empty($pass) && !empty($pass2)) {
         //проверка на совпадение паролей
@@ -36,21 +36,21 @@ if (isset($_POST['reg'])) {
 <html>
     <head>
         <meta charset="UTF-8">
+        <link href="../style/css.css" rel="stylesheet" type="text/css"/>
         <title>Регистрация</title>
     </head>
     <body>
+        <div id="reg">
+            <h3>Форма регистрации</h3>
+            <form method="POST">
+                <input type="text" name="username" placeholder="Логин">
+                <input type="email" name="email" placeholder="Email">
+                <input type="password" name="pass" placeholder="Пароль">
+                <input type="password" name="pass2" placeholder="Пароль повторно">
+                <input type="submit" name="reg" value="РЕГИСТРАЦИЯ">
+            </form>
+            <p class="error"><?= $mes ?></p>
+        </div>
 
-        <form method="POST">
-            <label for="username">Введите ваш логин:</label>
-            <input type="text" name="username">
-            <label for="username">Введите ваш email:</label>
-            <input type="email" name="email">
-            <label for="pass">Введите ваш пароль:</label>
-            <input type="password" name="pass">
-            <label for="pass">Введите ваш пароль еще раз:</label>
-            <input type="password" name="pass2">
-            <input type="submit" name="reg" value="РЕГИСТРАЦИЯ">
-        </form>
-        <p class="error"><?= $mes ?></p>
     </body>
 </html>
