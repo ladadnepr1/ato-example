c0d1137acc0444cb50b1ceae610e49c2a1f6e857
ladadnepr1 <ladadnepr1@gmail.com>
2017-07-26 20:11:00 +0300

    get_text_userid.php

--- functions/get_text_userid.php
+++ functions/get_text_userid.php
@@ -5,5 +5,5 @@
 function show_text_username($db,$username){
     $query = 'SELECT informs.name,informs.inform,informs_users.readinform FROM informs INNER JOIN informs_users ON informs.id=informs_users.inform_id INNER JOIN users ON users.id=informs_users.user_id WHERE users.name="'.$username.'"';
     return mysqli_query($db, $query);
-    
+ 
 }
