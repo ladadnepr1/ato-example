6f7fac0ac5cc8e89c234185f9b0a611b7f876f4a
Someone <rgbjkbs@hdbf.ru>
2017-07-26 14:22:12 +0300

    почти функция

--- functions/admin_functions.php
+++ functions/admin_functions.php
@@ -1,9 +1,16 @@
 <?php
 echo 'file1';
-//
-//const DB_USER = 'root';
-//const DB_PASS = '';
-//// вставить имя базы данных
-//const DB_NAME = 'inform_ato_web';
-//const DB_HOST = 'localhost';
-//
+
+const DB_USER = 'root';
+const DB_PASS = 'inform_ato_web';
+// вставить имя базы данных
+const DB_NAME = 'inform_ato_web';
+const DB_HOST = 'localhost';
+
+function rights($iduser){
+    if($iduser=='user'){
+        
+    }
+    $query="UPDATE". DB_NAME." SET`admin_is` = '1' WHERE `id` = $iduser";
+    
+}
