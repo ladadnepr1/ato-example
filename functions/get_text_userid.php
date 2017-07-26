b3ccb338e45912fcdc8add53373173dfe0b17886
ladadnepr1 <ladadnepr1@gmail.com>
2017-07-26 18:54:05 +0300

    username

--- functions/get_text_userid.php
+++ functions/get_text_userid.php
@@ -1,9 +1,9 @@
 <?php
-include_once '../includes/config.php';
+
 //функция принимает базу данных и номер пользователя
 // возвращает выборку заголовки и текст сообщений для пользователя id
-function show_text_userid($db,$userid){
-    $query = 'SELECT informs.name,informs.inform,informs_users.readinform FROM informs INNER JOIN informs_users ON informs.id=informs_users.inform_id WHERE informs_users.user_id'.$userid;
+function show_text_username($db,$username){
+    $query = 'SELECT informs.name,informs.inform,informs_users.readinform FROM informs INNER JOIN informs_users ON informs.id=informs_users.inform_id INNER JOIN users ON users.id=informs_users.user_id WHERE users.name="'.$username.'"';
     return mysqli_query($db, $query);
     
 }
