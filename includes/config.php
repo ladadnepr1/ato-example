5e84473b34c68e669ff42786aac575a60c5f3152
AndriyVolosh <andriy.volosh@gmail.com>
2017-07-26 11:07:55 +0300

    +Проверка соединения с БД +Исправление путей +array_slice(scandir...

--- includes/config.php
+++ includes/config.php
@@ -6,11 +6,19 @@ const DB_PASS = '';
 const DB_NAME = 'inform_ato_web';
 const DB_HOST = 'localhost';
 $db = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-$path_functions=$_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'functions';
-$files=scandir($path_functions);
+//Проверка соединения с БД
+if (!$db) {
+    echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL;
+    echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+//
+$path_functions=$_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'functions'.DIRECTORY_SEPARATOR;
+$files = array_slice(scandir($path_functions), 2);
 if($files){
     foreach($files as $value){
-        include_once $value;
+        include_once $path_functions.$value;
     }
 }
 
