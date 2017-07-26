87e983a15b7d25edce00978bfb9a7bfb04b594fd
ladadnepr1 <ladadnepr1@gmail.com>
2017-07-26 22:08:50 +0300

    show_message.php

--- includes/show_message.php
+++ includes/show_message.php
@@ -2,16 +2,24 @@
 include_once 'config.php';
 //$username=$_SESSION['username'];
 //<content>
-$inform= filter_input(INPUT_POST, 'inform');
-$name=filter_input(INPUT_POST, 'name');
+$inform_name = filter_input(INPUT_GET, 'name');
+if (is_null($inform_name)){
+    echo 'ВЫБЕРИТЕ СООБЩЕНИЕ'; 
+}else if($inform=get_inform($db,$username,$inform_name)){
+    echo $inform;
+}else{
+    echo 'problem2:' . mysqli_errno($db) . ' ' . mysqli_error($db);
+}
 
 ?>
-<div class="username">
-    <h3>user</h3>
-</div>
-<div class="title">
-    <p><?= $name ?></p>
-</div>
-<div class="text">
-    <p> <?= $inform ?> </p>
-</div>
+<content>
+    <div class="username">
+        <h3>user</h3>
+    </div>
+    <div class="title">
+        <p><?= $inform_name ?></p>
+    </div>
+    <div class="text">
+        <p> <?= $inform ?> </p>
+    </div>
+</content>
