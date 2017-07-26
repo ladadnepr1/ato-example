078b8bbececa6081c8bee4bc3cc25de4db487043
ladadnepr1 <ladadnepr1@gmail.com>
2017-07-26 16:26:01 +0300

    connect db

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,11 @@
 <?php
-
+// соединение с бд
 const DB_USER = 'root';
 const DB_PASS = '';
-// вставить имя базы данных
-const DB_NAME = '';
+const DB_NAME = 'inform_ato_web';
 const DB_HOST = 'localhost';
+$db = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+//присоединение всех файлов с функциями
 $path_functions=$_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'functions';
 $files=scandir($path_functions);
 if($files){
@@ -13,7 +14,6 @@ if($files){
     }
 }
 $db = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-//присоединение всех файлов с функциями
 $path_functions=$_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'functions/';
 $files=scandir($path_functions);
 if($files){
