bc98dbecc9102e075cdbfa81d265994b26c61467
AndriyVolosh <andriy.volosh@gmail.com>
2017-07-26 11:00:46 +0300

    add DB name

--- includes/config.php
+++ includes/config.php
@@ -3,7 +3,7 @@
 const DB_USER = 'root';
 const DB_PASS = '';
 // вставить имя базы данных
-const DB_NAME = '';
+const DB_NAME = 'inform_ato_web';
 const DB_HOST = 'localhost';
 $db = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 $path_functions=$_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'functions';
