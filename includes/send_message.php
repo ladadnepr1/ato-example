90c5371454804fb8e70e6603722d2f1b502fd6df
AndriyVolosh <andriy.volosh@gmail.com>
2017-07-26 11:30:38 +0300

    add send_message.php

--- includes/send_message.php
+++ includes/send_message.php
@@ -3,10 +3,10 @@ include_once 'config.php';
 
 //принимаем значения с POST
 if (isset($_POST['reg'])) {
-    $name = mysqli_real_escape_string($dbc, trim($_POST['username']));
-    $email = mysqli_real_escape_string($dbc, trim($_POST['email']));
-    $pass = mysqli_real_escape_string($dbc, trim($_POST['pass']));
-    $pass2 = mysqli_real_escape_string($dbc, trim($_POST['pass2']));
+    $name = mysqli_real_escape_string($db, trim($_POST['username']));
+    $email = mysqli_real_escape_string($db, trim($_POST['email']));
+    $pass = mysqli_real_escape_string($db, trim($_POST['pass']));
+    $pass2 = mysqli_real_escape_string($db, trim($_POST['pass2']));
 
     //проверка на пустые поля
     if (!empty($name) && !empty($email) && !empty($pass) && !empty($pass2)) {
@@ -14,12 +14,12 @@ if (isset($_POST['reg'])) {
         if ($pass == $pass2) {
             //проверка на совпадение логина
             $query = "SELECT * FROM `users` WHERE name = '$name'";
-            $data = mysqli_query($dbc, $query);
+            $data = mysqli_query($db, $query);
             if (mysqli_num_rows($data) == 0) {
                 $query = "INSERT INTO `users` (name,email, pass) VALUES ('$name','$email','$pass')";
                 mysqli_query($dbc, $query);
                 $sign = "<a href='signup.php'>>>>АВТОРИЗАЦИЯ<<<</a>";
-                $mes = 'Сообщение отправлено успешно!';
+                $mes = 'Регистрация Прошла успешно!';
                 mysqli_close($dbc);
             } else {
                 $mes_er = 'Логин уже существует';
@@ -37,19 +37,20 @@ if (isset($_POST['reg'])) {
     <head>
         <meta charset="UTF-8">
         <link href="../style/style.css" rel="stylesheet" type="text/css"/>
-        <title>Регистрация</title>
+        <title>Отправка сообщения</title>
     </head>
     <body>
         <div id="reg">
-            <h3>Форма регистрации</h3>
+            <h3>Форма отправки сообщения</h3>
             <p class="err"><?= $mes_er ?></p>
             <p class="norm"><?= $mes ?></p>
             <form method="POST">
-                <input type="text" name="username" placeholder="Логин">
-                <input type="email" name="email" placeholder="Email">
-                <input type="password" name="pass" placeholder="Пароль">
-                <input type="password" name="pass2" placeholder="Пароль повторно">
-                <input type="submit" name="reg" value="РЕГИСТРАЦИЯ">
+                <label>Выберите пользователей:
+                    <select size="1" multiple name="users">
+                        <option><?php ?> User1</option>
+                    </select>
+                </label><br/><br/>
+                <input type="submit" name="reg" value="Отправить сообщение">
             </form>
             <div class="signup"><?= $sign ?></div>
         </div>
