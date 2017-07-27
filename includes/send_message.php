37562de75e7852ba0ae296a4c6afcccb0b37c59c
dontTochMyNameFuckingBoy <maraffonskiy@gmail.com>
2017-07-27 16:31:19 +0300

    send_message.php

--- includes/send_message.php
+++ includes/send_message.php
@@ -36,6 +36,7 @@ if ($_GET) {
 
         //получаем user_id
         $query = "SELECT id FROM users WHERE name='$user'";
+	echo $user;
         $result4 = mysqli_query($db, $query);
         if (!$result4) {
             $mes_error .= 'not result' . ' ' . mysqli_errno($db) . ' ' . mysqli_error($db);
