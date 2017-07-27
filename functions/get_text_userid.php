47b0bcd6c554006fa966e0cc339e794257e3e8da
user <user@user.com>
2017-07-27 15:28:52 +0300

    добавил функцию unread_messages проверка не прочитанных сообщений

--- functions/get_text_userid.php
+++ functions/get_text_userid.php
@@ -16,41 +16,47 @@ function get_inform($db, $username, $inform_id) {
     $result = mysqli_query($db, $query_informs_users_id);
     $array = mysqli_fetch_assoc($result);
     if (is_null($array['readinform'])) {
-        if (is_null($array['id'])) {
-            echo 'problem3:' . mysqli_errno($db) . ' ' . mysqli_error($db);
-        } else {
-            $query_readinform = "UPDATE informs_users SET readinform = 1 WHERE informs_users.id = '".$array['id']."'";
-            $res=mysqli_query($db, $query_readinform);
-        }
+	if (is_null($array['id'])) {
+	    echo 'problem3:' . mysqli_errno($db) . ' ' . mysqli_error($db);
+	} else {
+	    $query_readinform = "UPDATE informs_users SET readinform = 1 WHERE informs_users.id = '" . $array['id'] . "'";
+	    $res = mysqli_query($db, $query_readinform);
+	}
     }
 
     //запрос на получение сообщения по inform_id
     $query_informs_users_id = "SELECT informs.inform,informs.name FROM informs WHERE informs.id='" . $inform_id . "'";
     return mysqli_query($db, $query_informs_users_id);
 }
+
 //код Валерия+
 //функция проверяет есть ли username в $_SESSION['username'].
 //Возвращает имя или false
-function username_SESSION ($username)
-{
-	$username = Fix($_POST['username']);//имя пользователя
-        if (isset($_SESSION['USERNAME'])) 
-		{
-            $username=$_SESSION['USERNAME'];//устанавливает, совпадает ли имя пользователя с сессионным 
-			echo "Добропожаловаль .$username";// die ("<p><a href='continue.php'>Щелкните здесь для продолжения</a></p>");
-        }else//return FALSE;
-		{
-			echo "Пожалуйста, для входа <a href='authenticate.php'> щелкните здесь</a>";
-		}
-	session_write_close(); //закрытие сессии
+function username_SESSION($username) {
+    $username = Fix($_POST['username']); //имя пользователя
+    if (isset($_SESSION['USERNAME'])) {
+	$username = $_SESSION['USERNAME']; //устанавливает, совпадает ли имя пользователя с сессионным 
+	echo "Добропожаловаль .$username"; // die ("<p><a href='continue.php'>Щелкните здесь для продолжения</a></p>");
+    } else {//return FALSE;
+	echo "Пожалуйста, для входа <a href='authenticate.php'> щелкните здесь</a>";
+    }
+    session_write_close(); //закрытие сессии
 }
-	 
-function Fix($str) //очистка полей
-{ 
-	$str = trim($str);
-	if(get_magic_quotes_gpc()) 
-	{
-		$str = stripslashes($str);//Удаляет экранирование символов, произведенное функцией addslashes()
-	}
-	return mysql_real_escape_string($str);
+
+function Fix($str) { //очистка полей
+    $str = trim($str);
+    if (get_magic_quotes_gpc()) {
+	$str = stripslashes($str); //Удаляет экранирование символов, произведенное функцией addslashes()
+    }
+    return mysql_real_escape_string($str);
+}
+
+/**
+ * проверка не прочитанных сообщений
+ * @param type $username
+ * @return type
+ */
+function unread_messages($username) {
+    $query = "select COUNT(*) FROM informs_users INNER JOIN users ON informs_users.user_id=users.id WHERE users.name='" . $username . "' AND informs_users.readinform IS NULL";
+    return $count_inform = mysqli_query(DB_NAME, $query);
 }
