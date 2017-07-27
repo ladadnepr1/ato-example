ed0c41679ff61454b714cc1e35e53177019d7c04
ladadnepr1 <ladadnepr1@gmail.com>
2017-07-27 10:10:31 +0300

    add Antoine_de_Saint-Exupery

--- includes/show_reg_aut.php
+++ includes/show_reg_aut.php
@@ -2,46 +2,34 @@
 //страница регистрации или авторизации
 
 include_once 'config.php';
-
 ?>
-<!DOCTYPE html>
-<html lang="ru">
-    <head>
-        <meta charset="UTF-8">
-        <link href="../style/style.css" rel="stylesheet" type="text/css"/>
-        <link rel="shortcut icon" href="../images/favicon.ico" type="image/x-icon">
-        <title>Сообщения</title>
-    </head>
-    <body>
-        <header>
-            <h1>Сообщения пользователя</h1>
-        </header>
-        <main>
-            <div id="lsb">
-                <div class="not_read">
-                    <ul>
-                        <li>
-                            <a href="includes/reg.php">АВТОРИЗАЦИЯ</a>
-                        </li>
-                        <li>
-                            <a href="includes/regisration.php">РЕГИСТРАЦИЯ</a>
-                        </li>
-                    </ul>
-                </div>
-                
-            </div>
-            <content>
-                <div class="username">
-                    <h3>Самый лучший messenger</h3>
-                </div>
-                <div class="title">
-                   
-                </div>
-                <div class="text">
-                    <p>Для входа введите свой логин и пароль.</p>
- 
-                </div>
-            </content>
-        </main>
-    </body>
-</html>
+<header>
+    <h1>Сообщения пользователя</h1>
+</header>
+<main>
+    <div id="lsb">
+        <div class="not_read">
+            <ul>
+                <li>
+                    <a href="includes/reg.php">АВТОРИЗАЦИЯ</a>
+                </li>
+                <li>
+                    <a href="includes/regisration.php">РЕГИСТРАЦИЯ</a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+    <content>
+        <div class="username">
+            <h3>Для входа введите свой логин и пароль.</h3>
+        </div>
+        <div class="title">
+
+        </div>
+        <div class="text">
+            <p>Единственная настоящая роскошь — это роскошь человеческого общения.</p>
+            <a href="http://xn--e1afnj0c.xn--p1ai/author/Antoine_de_Saint-Exupery/">Антуан де Сент-Экзюпери</a>
+        </div>
+    </content>
+</main>
