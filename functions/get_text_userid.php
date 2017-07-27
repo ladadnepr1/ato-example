f076d57e08db6a86b10aef5f7b9e33a7d0e1752d
user <user@user.com>
2017-07-27 16:39:30 +0300

    поменял функцию

--- functions/get_text_userid.php
+++ functions/get_text_userid.php
@@ -56,7 +56,8 @@ function Fix($str) { //очистка полей
  * @param type $username
  * @return type
  */
-function unread_messages($username) {
+function unread_messages($db,$username) {
     $query = "select COUNT(*) FROM informs_users INNER JOIN users ON informs_users.user_id=users.id WHERE users.name='" . $username . "' AND informs_users.readinform IS NULL";
-    return $count_inform = mysqli_query(DB_NAME, $query);
+    return $count_inform = mysqli_query($db, $query);
 }
+
