3e131905247b5728ae9c4929ca4526e88f233f9c
ladadnepr1 <ladadnepr1@gmail.com>
2017-07-27 09:58:50 +0300

    add show_admin.php

---
+++ includes/show_admin.php
@@ -0,0 +1,15 @@
+<?php
+//страница отображения сообщений админу
+// $username=$login
+
+
+?>
+<header>
+
+    <h1>Администратор : <span><?= $username; ?></span>:</h1>
+    <a href="http://ato-example/includes/logout.php">ВЫЙТИ</a>
+
+</header>
+<main>
+    
+</main>
