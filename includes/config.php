089392f9d4f013a40c3d95e9db95147a307f80fa
ladadnepr1 <ladadnepr1@gmail.com>
2017-07-27 09:15:37 +0300

    index.php для сборки

--- includes/config.php
+++ includes/config.php
@@ -10,15 +10,15 @@ $db = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 $path_functions=$_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'functions/';
 $files=scandir($path_functions);
 if($files){
-    foreach($files as $value){
-        
+    foreach($files as $value){   
         if(!($value=='.'|| $value=='..' || is_null ($value))){
             include_once $path_functions.$value;
         }
     }
 }
 // открытие сессии 
-//session_start();
+session_start();
+//
 
 
 
--- index.php
+++ index.php
@@ -2,9 +2,25 @@
 //сборка всех частей
 //если есть логин в $_SESSION['username'] , определить польз или админ стр 14 Валерий
 
-//если польз , переход на страницу show.php стр 9 Лада
-//если админ , переход на страницу show_admin.php стр 4 Андрей
+     //если польз , переход на страницу show.php стр 9 Лада
+     //если админ , переход на страницу show_admin.php стр 4 Андрей
 
 //если нет логина, переход на страницу show_reg_aut.php стр 3 Алексей и стр 13 Ярослав
 
 //
+$thisway='';
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <link href="style/style.css" rel="stylesheet" type="text/css"/>
+        <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+        <title>Сообщения</title>
+    </head>
+    <body>
+        <?php include_once './includes/'.$thisway ; ?>
+        
+        
+    </body>
