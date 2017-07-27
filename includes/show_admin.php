9bba40e59b73c79861d30dd5e903696c7de6403d
ladadnepr1 <ladadnepr1@gmail.com>
2017-07-27 20:49:19 +0300

    add logout

--- includes/show_admin.php
+++ includes/show_admin.php
@@ -62,7 +62,7 @@ if ($_GET) {
 <header>
 
     <h1>Администратор : <span><?= $username; ?></span></h1>
-    <a href="http://ato-example/includes/logout.php">ВЫЙТИ</a>
+    <a href="../includes/logout.php">ВЫЙТИ</a>
 
 </header>
 <main>
