e1f2208a1ee7df9b436d1fc8572817ad775d4da6
ladadnepr1 <ladadnepr1@gmail.com>
2017-07-27 09:38:16 +0300

    add path_includes

--- includes/config.php
+++ includes/config.php
@@ -5,9 +5,11 @@ const DB_PASS = '';
 const DB_NAME = 'inform_ato_web';
 const DB_HOST = 'localhost';
 $db = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+// путь на файлы includes
+$path_includes= $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'includes'.DIRECTORY_SEPARATOR;
 //присоединение всех файлов с функциями
 
-$path_functions=$_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'functions/';
+$path_functions=$_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'functions'.DIRECTORY_SEPARATOR;
 $files=scandir($path_functions);
 if($files){
     foreach($files as $value){   
