45b520ce52535e000beac5e122bf45fded374dfe
ladadnepr1 <ladadnepr1@gmail.com>
2017-07-27 05:37:05 +0300

    add show.php

---
+++ includes/show.php
@@ -0,0 +1,25 @@
+<?php
+//страница отображения сообщений пользователю - не админу
+include_once 'config.php';
+//изменить пользователя!!!!!!
+//$username=$_SESSION['username'];
+ $username='lada';
+?>
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <link href="../style/style.css" rel="stylesheet" type="text/css"/>
+        <link rel="shortcut icon" href="../images/favicon.ico" type="image/x-icon">
+        <title>Сообщения</title>
+    </head>
+    <body>
+        <header>
+            <h1>Сообщения пользователя</h1>
+        </header>
+        <main>
+             <?php include_once 'show_text.php'; ?>
+             <?php include_once 'show_message.php'; ?>
+        </main>
+    </body>
+</html>
