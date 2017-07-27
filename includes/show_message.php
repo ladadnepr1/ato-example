2d48cdfdbd759e47b6dbc90672d51507710c03bd
ladadnepr1 <ladadnepr1@gmail.com>
2017-07-27 06:37:11 +0300

    show_message.php

--- includes/show_message.php
+++ includes/show_message.php
@@ -1,25 +1,28 @@
 <?php
 include_once 'config.php';
-//$username=$_SESSION['username'];
-//<content>
-$inform_name = filter_input(INPUT_GET, 'name');
-if (is_null($inform_name)){
-    echo 'ВЫБЕРИТЕ СООБЩЕНИЕ'; 
-}else if($inform=get_inform($db,$username,$inform_name)){
-    echo $inform;
-}else{
-    echo 'problem2:' . mysqli_errno($db) . ' ' . mysqli_error($db);
-}
 
+//<content>
+$inform_id = filter_input(INPUT_GET, 'inform_id');
+
+
+if (is_null($inform_id)) {
+    $mes = 'ВЫБЕРИТЕ СООБЩЕНИЕ';
+} else if ($inform = get_inform($db, $username, $inform_id)) {
+    $row = mysqli_fetch_assoc($inform);
+    $inform_name = $row['name'];
+    $mes = $row['inform'];
+} else {
+    $mes = 'problem2:' . mysqli_errno($db) . ' ' . mysqli_error($db);
+}
 ?>
 <content>
     <div class="username">
-        <h3>user</h3>
+        <h3> <?= $username ?></h3>
     </div>
     <div class="title">
         <p><?= $inform_name ?></p>
     </div>
     <div class="text">
-        <p> <?= $inform ?> </p>
+        <p> <?= $mes ?> </p>
     </div>
 </content>
