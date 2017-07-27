597c8a9b9722e8f839a435ce3a68f5c51c4e16fc
dontTochMyNameFuckingBoy <maraffonskiy@gmail.com>
2017-07-27 16:28:54 +0300

    functions/admin_functions.php

--- functions/admin_functions.php
+++ functions/admin_functions.php
@@ -12,49 +12,50 @@
  * @return string
  * админ или юзер
  */
-function is_admin( $username){
-    $query="SELECT  `admin_is` FROM `users` WHERE name=$username";
-    if(mysqli_query($db, $query)){
-        return true;
-    } else {
-        return false;
-    }       
-}
-
-
-
-
-/**
- * функция проверяет при помощи функции is_admin() права, и если 'user' - то изменяет их на 'admin', а если  'admin' - меняет на 'user'.При этом выводит сообщение об успехе или ошибке. 
- * 
- * @param int $iduser
- *  id пользователя
- */
-function rights(int $iduser){
-    $db= mysqli_connect(DB_HOST, DB_USER, DB_PASS,DB_NAME);
-if (!$db){
-    echo 'трабл при подключении к СУБД: '.mysqli_connect_error().' '. mysqli_connect_errno();
-    
-} else {
-    //echo 'подлючение успешно';
-    if(is_admin($iduser)=='user'){
-    $query="UPDATE `users` SET `admin_is` = '1' WHERE `id` = $iduser";
-    $result=mysqli_query($db, $query); 
-    if(!$result){
-        echo 'результат не был получен: '. mysqli_error($db).' '. mysqli_errno($db);
-}else{
-        echo 'права пользователя изменены на админские';
-    }
-    }elseif (is_admin($iduser)=='admin') {
-            $query="UPDATE". DB_NAME." SET `admin_is` = '0' WHERE `id` = $iduser";
-    $result=mysqli_query($db, $query); 
-    if(!$result){
-        echo 'результат не был получен: '. mysqli_error($db).' '. mysqli_errno($db);
-}else{
-        echo 'права админа изменены на пользователя';
-        }
-}
-}
-    
-}
-
+//function is_admin( $db,$username){
+//    $query="SELECT  `admin_is` FROM `users` WHERE name=$username";
+//    var_dump($username);
+//    if(mysqli_query($db, $query)){
+//        return true;
+//    } else {
+//        return false;
+//    }       
+//}
+//
+//
+//
+//
+///**
+// * функция проверяет при помощи функции is_admin() права, и если 'user' - то изменяет их на 'admin', а если  'admin' - меняет на 'user'.При этом выводит сообщение об успехе или ошибке. 
+// * 
+// * @param int $iduser
+// *  id пользователя
+// */
+//function rights($iduser){
+//    $db= mysqli_connect(DB_HOST, DB_USER, DB_PASS,DB_NAME);
+//if (!$db){
+//    echo 'трабл при подключении к СУБД: '.mysqli_connect_error().' '. mysqli_connect_errno();
+//    
+//} else {
+//    //echo 'подлючение успешно';
+//    if(is_admin($iduser)=='user'){
+//    $query="UPDATE `users` SET `admin_is` = '1' WHERE `id` = $iduser";
+//    $result=mysqli_query($db, $query); 
+//    if(!$result){
+//        echo 'результат не был получен: '. mysqli_error($db).' '. mysqli_errno($db);
+//}else{
+//        echo 'права пользователя изменены на админские';
+//    }
+//    }elseif (is_admin($iduser)=='admin') {
+//            $query="UPDATE". DB_NAME." SET `admin_is` = '0' WHERE `id` = $iduser";
+//    $result=mysqli_query($db, $query); 
+//    if(!$result){
+//        echo 'результат не был получен: '. mysqli_error($db).' '. mysqli_errno($db);
+//}else{
+//        echo 'права админа изменены на пользователя';
+//        }
+//}
+//}
+//    
+//}
+//
