6a8e7e04989f6be63ea91aa1990b596e0e1bab74
dontTochMyNameFuckingBoy <maraffonskiy@gmail.com>
2017-07-27 16:30:20 +0300

    logout.php

--- includes/logout.php
+++ includes/logout.php
@@ -8,7 +8,8 @@
 include_once 'config.php';
 $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-unset($_SESSION['ussername']);
-header('Location:http://ato-example/includes/signup.php');
+unset($_SESSION['username']);
+//header("Location:$path_includes.'index.php'");
+echo 'Вы вышли!<br> <a href="../index.php">ПЕРЕЙТИ НА ГЛАВНУЮ</a>';
 
 
