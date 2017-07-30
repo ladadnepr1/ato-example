602f1258e14132a6d2086c9fb5f72ebcef5db41f
ladadnepr1 <ladadnepr1@gmail.com>
2017-07-30 10:15:48 +0300

    change show_text.php

--- functions/get_text_userid.php
+++ functions/get_text_userid.php
@@ -2,8 +2,10 @@
 
 //функция принимает базу данных и номер пользователя
 // возвращает выборку заголовки и текст сообщений для пользователя id
-function show_text_username($db, $username) {
-    $query = 'SELECT informs.name,informs.inform,informs_users.readinform,informs_users.inform_id FROM informs INNER JOIN informs_users ON informs.id=informs_users.inform_id INNER JOIN users ON users.id=informs_users.user_id WHERE users.name="' . $username . '"';
+function show_text_username($db, $username,$page) {
+    $offset=$page*10;
+    $query = 'SELECT informs.name,informs.inform,informs_users.readinform,informs_users.inform_id FROM informs INNER JOIN informs_users ON informs.id=informs_users.inform_id INNER JOIN users ON users.id=informs_users.user_id WHERE users.name="' . $username . '" limit '.$offset.',10';
+    //$query = 'SELECT informs.name,informs.inform,informs_users.readinform,informs_users.inform_id FROM informs INNER JOIN informs_users ON informs.id=informs_users.inform_id INNER JOIN users ON users.id=informs_users.user_id WHERE users.name="' . $username . '"';
     return mysqli_query($db, $query);
 }
 
--- includes/show_text.php
+++ includes/show_text.php
@@ -1,15 +1,17 @@
 <?php 
 //<div id="lsb">
-$result = show_text_username($db, $username);
+$result = show_text_username($db, $username,0);
 if (!$result): echo 'Ошибка1: ' . mysqli_errno($db) . ' ' . mysqli_error($db); ?>
 
 <?php else : ?>
  <div id="lsb">
-        <div class="not_read">
+        <div >
+            <h3>непрочитанные:</h3>
+            
             <ul>
                 <?php while ($row = mysqli_fetch_assoc($result)): ?>
                     <?php if (is_null($row['readinform'])) : ?>
-                    <li >
+                    <li class="not_read">
                         <a href="index.php?inform_id=<?= $row['inform_id'] ; ?>"> <?= $row['name'] ; ?></a>
                     </li>
                     
@@ -19,9 +21,9 @@ if (!$result): echo 'Ошибка1: ' . mysqli_errno($db) . ' ' . mysqli_error($
             
         </div>
         <div class="read">
-            
+            <h3>прочитанные:</h3>
             <ul >
-                <?php $result = show_text_username($db, $username); ?>
+                <?php $result = show_text_username($db, $username,0); ?>
                  <?php while ($row = mysqli_fetch_assoc($result)): ?>
                 
                     <?php if (!is_null($row['readinform'])) : ?>
